e5e33fb27db98ad0739eb6dd75b74d9473639ff7
mbozwood <matthew.bozwood@gray-dawes.co.uk>
2017-05-23 11:21:28 +0100

    roles on sidebar

--- resources/views/layouts/nav/left.blade.php
+++ resources/views/layouts/nav/left.blade.php
@@ -44,54 +44,73 @@
             <a href="#"><i class="px-nav-icon fa fa-comment"></i><span class="px-nav-label">Feedback</span></a>
         </li>
         @if(Auth::check())
-        <li class="px-nav-item {{ active('forum.*', 'px-nav-active') }}">
-            <a href="#"><i class="px-nav-icon fa fa-forumbee"></i><span class="px-nav-label">Forum</span></a>
-        </li>
-        <li class="px-nav-item {{ active('ids.*', 'px-nav-active') }}">
-            <a href="#"><i class="px-nav-icon fa fa-microchip"></i><span class="px-nav-label">IDS</span></a>
-        </li>
-        <li class="px-nav-item px-nav-dropdown {{ active(['training.*','admin.training.*'], 'px-open') }}">
-            <a href="#"><i class="px-nav-icon fa fa-pencil"></i><span class="px-nav-label">Training</span></a>
-            <ul class="px-nav-dropdown-menu">
-                <li class="px-nav-item {{ active('training.availability', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">View Availability</span></a></li>
-                <li class="px-nav-item {{ active('training.history', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Session History</span></a></li>
-                <li class="px-nav-item px-nav-dropdown {{ active('admin.training.*', 'px-open') }}">
-                    <a href="#"><span class="px-nav-label">Instructional Staff</span></a>
+            <li class="px-nav-item {{ active('forum.*', 'px-nav-active') }}">
+                <a href="#"><i class="px-nav-icon fa fa-forumbee"></i><span class="px-nav-label">Forum</span></a>
+            </li>
+            <li class="px-nav-item {{ active('ids.*', 'px-nav-active') }}">
+                <a href="#"><i class="px-nav-icon fa fa-microchip"></i><span class="px-nav-label">IDS</span></a>
+            </li>
+            <li class="px-nav-item px-nav-dropdown {{ active(['training.*','admin.training.*'], 'px-open') }}">
+                <a href="#"><i class="px-nav-icon fa fa-pencil"></i><span class="px-nav-label">Training</span></a>
+                <ul class="px-nav-dropdown-menu">
+                    <li class="px-nav-item {{ active('training.availability', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">View Availability</span></a></li>
+                    <li class="px-nav-item {{ active('training.history', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Session History</span></a></li>
+                    @if(Auth::user()->hasRole(['atm','datm','ta','ins','mtr','ata']))
+                        <li class="px-nav-item px-nav-dropdown {{ active('admin.training.*', 'px-open') }}">
+                            <a href="#"><span class="px-nav-label">Instructional Staff</span></a>
+                            <ul class="px-nav-dropdown-menu">
+                                <li class="px-nav-item {{ active('admin.training.sessions.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Scheduled Sessions</span></a></li>
+                                <li class="px-nav-item {{ active('admin.training.availability.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Set Availability</span></a></li>
+                                <li class="px-nav-item {{ active('admin.training.studenthist.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">View Student's History</span></a></li>
+                                @if(Auth::user()->hasRole(['atm','datm','ta']))
+                                    <li class="px-nav-item {{ active('admin.training.solocert.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Solo Cert Admin</span></a></li>
+                                @endif
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </li>
+            @if(Auth::user()->hasRole(['atm','datm','ta','wm','ec','fe','ins','mtr','aec','awm','ata']))
+                <li class="px-nav-item px-nav-dropdown {{ active(['admin.*', 'not:admin.training.*'], 'px-open') }}">
+                    <a href="#"><i class="px-nav-icon fa fa-lock"></i><span class="px-nav-label">{{ $settings->artcc_code }} Administration</span></a>
                     <ul class="px-nav-dropdown-menu">
-                        <li class="px-nav-item {{ active('admin.training.sessions.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Scheduled Sessions</span></a></li>
-                        <li class="px-nav-item {{ active('admin.training.availability.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Set Availability</span></a></li>
-                        <li class="px-nav-item {{ active('admin.training.studenthist.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">View Student's History</span></a></li>
-                        <li class="px-nav-item {{ active('admin.training.solocert.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Solo Cert Admin</span></a></li>
+                        @if(Auth::user()->hasRole(['atm','datm','ta','wm','ins','mtr','ata']))
+                            <li class="px-nav-item {{ active('admin.roster.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Roster</span></a></li>
+                        @endif
+                        @if(Auth::user()->hasRole(['atm','datm','ta','wm']))
+                            <li class="px-nav-item {{ active('admin.staff.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Staff</span></a></li>
+                        @endif
+                        @if(Auth::user()->hasRole(['atm','datm','ec','aec','wm']))
+                            <li class="px-nav-item {{ active('admin.events.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Events</span></a></li>
+                        @endif
+                        @if(Auth::user()->hasRole(['atm','datm','fe','ta','wm']))
+                            <li class="px-nav-item {{ active('admin.documents.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Documents & Files</span></a></li>
+                        @endif
+                        @if(Auth::user()->hasRole(['atm','datm']))
+                        <li class="px-nav-item px-nav-dropdown {{ active('admin.admin.*', 'px-open') }}">
+                            <a href="#"><span class="px-nav-label">Other Administration</span></a>
+                            <ul class="px-nav-dropdown-menu">
+                                <li class="px-nav-item {{ active('admin.admin.broadcast.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Broadcast Email</span></a></li>
+                                <li class="px-nav-item {{ active('admin.admin.activity.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Site Activity</span></a></li>
+                                <li class="px-nav-item {{ active('admin.admin.feedback.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Feedback</span></a></li>
+                                <li class="px-nav-item {{ active('admin.admin.announcements.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Index Announcements</span></a></li>
+                            </ul>
+                        </li>
+                        @endif
                     </ul>
                 </li>
-            </ul>
-        </li>
-        <li class="px-nav-item px-nav-dropdown {{ active(['admin.*', 'not:admin.training.*'], 'px-open') }}">
-            <a href="#"><i class="px-nav-icon fa fa-lock"></i><span class="px-nav-label">{{ $settings->artcc_code }} Administration</span></a>
-            <ul class="px-nav-dropdown-menu">
-                <li class="px-nav-item {{ active('admin.roster.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Roster</span></a></li>
-                <li class="px-nav-item {{ active('admin.staff.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Staff</span></a></li>
-                <li class="px-nav-item {{ active('admin.events.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Events</span></a></li>
-                <li class="px-nav-item {{ active('admin.documents.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Documents & Files</span></a></li>
-                <li class="px-nav-item px-nav-dropdown {{ active('admin.admin.*', 'px-open') }}">
-                    <a href="#"><span class="px-nav-label">Other Administration</span></a>
+            @endif
+            @if(Auth::user()->hasRole(['atm','datm','wm']))
+                <li class="px-nav-item px-nav-dropdown {{ active('system.*', 'px-open') }}">
+                    <a href="#"><i class="px-nav-icon fa fa-lock"></i><span class="px-nav-label">System Administration</span></a>
                     <ul class="px-nav-dropdown-menu">
-                        <li class="px-nav-item {{ active('admin.admin.broadcast.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Broadcast Email</span></a></li>
-                        <li class="px-nav-item {{ active('admin.admin.activity.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Site Activity</span></a></li>
-                        <li class="px-nav-item {{ active('admin.admin.feedback.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Feedback</span></a></li>
-                        <li class="px-nav-item {{ active('admin.admin.announcements.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Index Announcements</span></a></li>
+                        <li class="px-nav-item {{ active('system.settings.*', 'px-nav-active') }}"><a href="{{ route('system.settings.index') }}"><span class="px-nav-label">Settings</span></a></li>
+                        <li class="px-nav-item {{ active('system.roles.*', 'px-nav-active') }}"><a href="{{ route('system.roles.index') }}"><span class="px-nav-label">Roles</span></a></li>
+                        <li class="px-nav-item {{ active('system.airports.*', 'px-nav-active') }}"><a href="{{ route('system.airports.index') }}"><span class="px-nav-label">Airports</span></a></li>
+                        <li class="px-nav-item {{ active('system.forum.*', 'px-nav-active') }}"><a href="#"><span class="px-nav-label">Forum Admin</span></a></li>
                     </ul>
                 </li>
-            </ul>
-        </li>
-        <li class="px-nav-item px-nav-dropdown {{ active('system.*', 'px-open') }}">
-            <a href="#"><i class="px-nav-icon fa fa-lock"></i><span class="px-nav-label">System Administration</span></a>
-            <ul class="px-nav-dropdown-menu">
-                <li class="px-nav-item {{ active('system.settings.*', 'px-nav-active') }}"><a href="{{ route('system.settings.index') }}"><span class="px-nav-label">Settings</span></a></li>
-                <li class="px-nav-item {{ active('system.roles.*', 'px-nav-active') }}"><a href="{{ route('system.roles.index') }}"><span class="px-nav-label">Roles</span></a></li>
-                <li class="px-nav-item {{ active('system.airports.*', 'px-nav-active') }}"><a href="{{ route('system.airports.index') }}"><span class="px-nav-label">Airports</span></a></li>
-            </ul>
-        </li>
+            @endif
         @endif
     </ul>
 </nav>
